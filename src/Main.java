c74656642a997349379d5ae9a01ef6ffb1e08ea4
Xymdyx <samford892@gmail.com>
2022-03-09 21:46:16 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/* author: stf8464
+* desc: project 2 mail
+* instructor: TJ Borreli
+* Due date: 3/17/22
+* */
+
+public class Main{
+    public static int main(String[] args)
+    {
+        return 0;
+    }
+}
